d31fc8f36aa36439499940e7f133bf1580a73b30
pramodshenkar2210@gmail.com <pramodshenkar2210@gmail.com>
2022-05-06 00:28:19 +0530

    hello world with failed test case

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var version = "dev"
+
+func main() {
+	fmt.Println("version: ", version)
+	fmt.Println(sayHello())
+}
+
+func sayHello() string {
+	return "Hello Glang!"
+}
